f290e7e50a2db37e14be323514e65a1a98d0d381
EstefaniaRomero <estefaniaromero.torrents@gmail.com>
2022-01-28 14:00:59 +0100

    Initial commit

---
+++ src/ArraysNombre.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ArraysNombre {
+
+    public static void main(String[] args) {
+
+        String [] nom1 = new String[nom1.length];
+        String [] nom2 = new String[nom2.length];
+        String nom1;
+        String nom2;
+        Scanner sc = new Scanner(System.in);
+
+
+            System.out.println("Introdueix un primer nom: " );
+            nom1 = sc.nextLine();
+            System.out.println("Introdueix un segon nom: " );
+            nom2 = sc.nextLine();
+    }
+
+
+}
